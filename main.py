4913b6511ed0ade472bab25378b44f5ea0d4c296
Pal <105495138+pal404blog@users.noreply.github.com>
2023-03-11 17:56:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# ### Import dependencies
 
+# %%
+from transformers import GPT2LMHeadModel , GPT2Tokenizer
+
+# %% [markdown]
+# ### Load Model
+
+# %%
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
+model = GPT2LMHeadModel.from_pretrained('gpt2-large' , pad_token_id = tokenizer.eos_token_id )
 
-model = GPT2LMHeadModel.from_pretrained('gpt2-large', pad_token_id=tokenizer.eos_token_id)
-model.eval()
 
-initial_text = r 'Data Science and Machine Learning'
-input_tokens= tokenizer(initial_text, return_tensors='pt').input_ids
+# %%
+tokenizer.decode(tokenizer.eos_token_id)
 
-print("Output:\n" + 100 * '-')
-content = model.generate(input_tokens, penalty_alpha=0.6, top_k=4, max_length=512)
-print("" + 100 * '-')
+# %% [markdown]
+# ### Tokenize the text
+
+# %%
+sentence = "Love is"
+input_ids = tokenizer.encode(sentence , return_tensors = 'pt')
+
+
+# %%
+input_ids
+
+
+# %%
+tokenizer.decode(input_ids[0])
+
+
+# %%
+print(tokenizer.decode(input_ids[0][1]))
+print(tokenizer.decode(input_ids[0][2]))
+print(tokenizer.decode(input_ids[0][3]))
+print(tokenizer.decode(input_ids[0][4]))
+
+# %% [markdown]
+# ### Generate and Decode Text
+
+# %%
+output = model.generate(input_ids, max_length = 500, num_beams = 5,no_repeat_ngram_size  = 2 , early_stopping = True)
+
+
+# %%
+output[0]
+
+
+# %%
+print(tokenizer.decode(output[0] , skip_special_tokens = True))
+
+# %% [markdown]
+# ### Output Result
+
+# %%
+text = tokenizer.decode(output[0] , skip_special_tokens = True)
+
+
+# %%
+with open('blog_ai.txt','w') as f:
+    f.write(text)
+
+
